44ee660f5095c5f39564b7201f4bb5bef47150d1
wdddo <webdesigndevelopment.dortmund@gmail.com>
2025-06-20 21:06:30 +0200

    mit menü

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function(d, s) {
     var js, rjs = d.getElementsByTagName(s)[0];
     js = d.createElement(s);
-    js.src = "//static.resmio.com/static/de/widget.js#id=test-335&height=600&width=400&fontSize=14px";
+    js.src = "//static.resmio.com/static/de/widget.js#id=test-335&height=550&width=500&fontSize=14px";
     rjs.parentNode.insertBefore(js, rjs);
 }(document, "script"));
