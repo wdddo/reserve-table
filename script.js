471caea7db6104cadf329dca72d2ce2c3e41ba2a
wdddo <webdesigndevelopment.dortmund@gmail.com>
2025-06-20 20:14:19 +0200

    initial

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 (function(d, s) {
-  var js, rjs = d.getElementsByTagName(s)[0];
-  js = d.createElement(s);
-  js.src = "//static.resmio.com/static/de/button.js";
-  js.async = true;
-  rjs.parentNode.insertBefore(js, rjs);
-})(document, "script");
+    var js, rjs = d.getElementsByTagName(s)[0];
+    js = d.createElement(s);
+    js.src = "//static.resmio.com/static/de/widget.js#id=test-335&height=600&width=400&fontSize=14px";
+    rjs.parentNode.insertBefore(js, rjs);
+}(document, "script"));
