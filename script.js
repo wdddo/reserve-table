5187cb31e21f0f9fcd0c16810908f7a66d7944f8
wdddo <webdesigndevelopment.dortmund@gmail.com>
2025-06-20 19:40:00 +0200

    button

---
+++ script.js
@@ -0,0 +1,7 @@
+(function(d, s) {
+  var js, rjs = d.getElementsByTagName(s)[0];
+  js = d.createElement(s);
+  js.src = "//static.resmio.com/static/de/button.js";
+  js.async = true;
+  rjs.parentNode.insertBefore(js, rjs);
+})(document, "script");
